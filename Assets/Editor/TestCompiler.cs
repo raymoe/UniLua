c3a6a2ae22c9e9829cd3f64781d369cf915dc3ea
rayz <rayz888@163.com>
2018-08-24 22:36:42 +0800

    add an dump test

---
+++ Assets/Editor/TestCompiler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Editor
+{
+    using UnityEngine;
+    using UnityEditor;
+    using UniLua.Tools;
+    using System;
+    public class TestCompiler 
+    {
+        [MenuItem("LuaTools/TestCompileToProto")]
+        public static void TestCompileToProto()
+        {
+            var proto = Compiler.CompileFile(string.Format("{0}/LuaScript/{1}", Application.streamingAssetsPath, "test1.lua"));
+            Compiler.DumpingToFile(proto,string.Format("{0}/LuaScript/{1}", Application.streamingAssetsPath, "test1.proto"),true);
+        }
+    }
+}
